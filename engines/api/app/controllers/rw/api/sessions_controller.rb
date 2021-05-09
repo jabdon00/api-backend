ac71b5b863b780843029aa37a73f8321b812c907
j.donyavi <j.donyavi@J-DONYAVI>
2021-05-09 10:05:16 +0430

    some fixes

--- engines/api/app/controllers/rw/api/sessions_controller.rb
+++ engines/api/app/controllers/rw/api/sessions_controller.rb
@@ -4,7 +4,7 @@ module Rw
       skip_before_action :verify_authenticity_token
 
       def create
-        user = User.find_by_email(sign_in_params[:email])
+        user = Rw::User.find_by_email(sign_in_params[:email])
 
         if user && user.valid_password?(sign_in_params[:password])
           token = user.generate_jwt
